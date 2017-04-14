8c37a5c2b2109ff6a70d9bcf16e91e05d13f97ff
ingamontclair <gaidukovai1@montclair.edu>
2017-04-14 11:38:51 -0400

    Initial commit

---
+++ app/src/main/java/edu/montclair/mobilecomputing/m_alrajab/week11_firebase_chatapp/model/ChatMessageItem.java
@@ -0,0 +1,51 @@
+package edu.montclair.mobilecomputing.m_alrajab.week11_firebase_chatapp.model;
+
+/**
+ * Created by m_alrajab on 4/3/17.
+ */
+
+public class ChatMessageItem {
+
+    private String msgBody;
+    private String senderName;
+    private String uri;
+    private String timeStamp;
+
+    public ChatMessageItem(){
+
+    }
+
+
+    // this is today design
+    public ChatMessageItem(String msgBody, String senderName, String time, String u) {
+        this.msgBody = msgBody;
+        this.senderName = senderName;
+        this.timeStamp = time;
+        this.uri = u;
+    }
+
+
+    public String getMsgBody() {
+        return msgBody;
+    }
+
+    public void setMsgBody(String msgBody) {
+        this.msgBody = msgBody;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getPhotoUrl() {
+        return uri;
+    }
+}
